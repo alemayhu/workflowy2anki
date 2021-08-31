c1b87ad0fb46ae9549070e8a0f3e88b5c593189e
Alexander Alemayhu <alexander@alemayhu.com>
2021-08-31 12:55:20 +0200

    test: setup stubs

--- test.js
+++ test.js
@@ -8,3 +8,16 @@ test('bar', async t => {
 	const bar = Promise.resolve('bar');
 	t.is(await bar, 'bar');
 });
+
+
+test.skip('formatted', t => {
+	t.fail("to be implemented");
+});
+
+test('Plain text', t => {
+	t.fail("to be implemented");
+});
+
+test.skip('OPML', t => {
+	t.fail("to be implemented");
+});
